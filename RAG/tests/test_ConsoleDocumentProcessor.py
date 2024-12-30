4d6759d5e1665f7e760f1b2dc8d5bf55685d5d4a
Corso <58090876+yosbel-penate@users.noreply.github.com>
2024-12-30 14:16:56 -0500

    se creo un unittest para ConsoleDocumentProcessor

---
+++ RAG/tests/test_ConsoleDocumentProcessor.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.mock import MagicMock
+from RAG.src.user.ConsoleDocumentProcessor import ConsoleDocumentProcessor
+from RAG.src.vector_store.ChromaVectorStoreAdapter import ChromaVectorStoreAdapter
+
+class TestConsoleDocumentProcessor(unittest.TestCase):
+    def setUp(self):
+        self.vector_store_adapter = ChromaVectorStoreAdapter()
+        self.vector_store_adapter.index_documents = MagicMock()
+        self.processor = ConsoleDocumentProcessor(self.vector_store_adapter, ["Documento 1", "Documento 2"])
+
+    def test_process_documents(self):
+        self.processor.process_documents()
+        self.vector_store_adapter.index_documents.assert_called_once_with(["Documento 1", "Documento 2"])
+
+if __name__ == '__main__':
+    unittest.main()
