f88c7e2f697facb08f4b678f30fa8adb7dce7d7e
Corso <58090876+yosbel-penate@users.noreply.github.com>
2024-12-30 07:27:50 -0500

    se ajusto app

--- RAG/src/app.py
+++ RAG/src/app.py
@@ -4,7 +4,7 @@ from RAG.src.input.SimpleInputAdapter import SimpleInputAdapter
 from RAG.src.output.SimpleOutputAdapter import SimpleOutputAdapter
 from RAG.src.retrieval.SimpleRetrievalAdapter import SimpleRetrievalAdapter
 from RAG.src.user.ProcessRequestAdapter import ProcessRequestAdapter
-from RAG.src.vector_store.ChromaVectorStoreAdapter import SimpleVectorStoreAdapter
+from RAG.src.vector_store.ChromaVectorStoreAdapter import ChromaVectorStoreAdapter
 from RAG.src.language_model.GeminiLanguageModelAdapter import (
     GeminiLanguageModelAdapter
 )
@@ -14,6 +14,6 @@ if __name__ == "__main__":
                             retrieval_adapter = SimpleRetrievalAdapter(),
                             generation_adapter = SimpleGenerationAdapter(GeminiLanguageModelAdapter()),
                             output_adapter = SimpleOutputAdapter(),
-                            vector_store_adapter = SimpleVectorStoreAdapter(),
+                            vector_store_adapter = ChromaVectorStoreAdapter(),
                             document_source_adapter = SimpleDocumentSourceAdapter())
     adapter.run("¿Cuál es la capital de Francia?")
