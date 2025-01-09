09c2c57179676abe8df7b6a3806961c993c4e4b6
Corso <58090876+yosbel-penate@users.noreply.github.com>
2025-01-09 17:05:08 -0500

    se crea el archivo con los fracmentos

--- chunk_task/monitor.py
+++ chunk_task/monitor.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 def get_txt_files(directory):
     txt_files = []
@@ -21,15 +22,21 @@ def chunk_text(text, chunk_size=5000, overlap=1000):
         start += chunk_size - overlap
     return chunks
 
+def save_chunks_to_file(chunks, output_file):
+    chunks_json = {"docs": [{"doc": chunk} for chunk in chunks]}
+    with open(output_file, 'w', encoding='utf-8') as file:
+        json.dump(chunks_json, file, ensure_ascii=False, indent=4)
+
 def main():
-    directory = './documents'
+    current_dir = os.path.dirname(__file__)
+    directory = os.path.join(current_dir, 'documents')
     txt_files = get_txt_files(directory)
     all_chunks = []
     for file_path in txt_files:
         text = read_file(file_path)
         chunks = chunk_text(text)
         all_chunks.extend(chunks)
-    # Aquí puedes hacer algo con all_chunks, como guardarlos en un archivo o procesarlos
+    save_chunks_to_file(all_chunks, os.path.join(current_dir, 'chunk.json'))
     print(f"Total de fragmentos: {len(all_chunks)}")
 
 if __name__ == "__main__":
