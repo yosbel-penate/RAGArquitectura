8599ab0e1f12df7d1873b26f5399734c27ec9cea
Corso <58090876+yosbel-penate@users.noreply.github.com>
2025-01-09 17:16:33 -0500

    los docuementos se cargar del archivo churk.json

--- app_console.py
+++ app_console.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 # Configurar logging
 logging.basicConfig(level=logging.INFO)
@@ -12,11 +13,10 @@ if __name__ == "__main__":
     vector_store_adapter = ChromaVectorStoreAdapter()
     output_adapter = ConsoleOutputAdapter()
 
-    # Añadir documentos a la colección
-    documentos = [
-        "París es la capital de Francia.",
-        "Berlín es la capital de Alemania.",
-        "Madrid es la capital de España."
-    ]
+    # Cargar documentos desde el archivo JSON y convertirlos a una lista de strings
+    with open('chunk_task/chunk.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        documentos = [doc['doc'] for doc in data['docs']]
+
     processor = ConsoleDocumentProcessor(vector_store_adapter, documentos, output_adapter=output_adapter)
-    processor.process_documents_and_get_answer("¿Cuál es la capital de Alemania?")
+    processor.process_documents_and_get_answer("dame 1 problema de programacion")
