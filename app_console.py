8e460a6478f663cd9835fab63ea99b689bd48196
Corso <58090876+yosbel-penate@users.noreply.github.com>
2025-01-09 17:17:42 -0500

    refractoring load docs from file

--- app_console.py
+++ app_console.py
@@ -9,14 +9,17 @@ from RAG.src.vector_store.ChromaVectorStoreAdapter import ChromaVectorStoreAdapt
 from RAG.src.output.ConsoleOutputAdapter import ConsoleOutputAdapter
 
 
+def load_docs_from_json_file():
+    with open('chunk_task/chunk.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        documentos = [doc['doc'] for doc in data['docs']]
+    return documentos
+
 if __name__ == "__main__":
     vector_store_adapter = ChromaVectorStoreAdapter()
     output_adapter = ConsoleOutputAdapter()
 
-    # Cargar documentos desde el archivo JSON y convertirlos a una lista de strings
-    with open('chunk_task/chunk.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        documentos = [doc['doc'] for doc in data['docs']]
+    documentos = load_docs_from_json_file()
 
     processor = ConsoleDocumentProcessor(vector_store_adapter, documentos, output_adapter=output_adapter)
     processor.process_documents_and_get_answer("dame 1 problema de programacion")
